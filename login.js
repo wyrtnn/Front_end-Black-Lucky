88f3b129a72e4604db2e2bd4e6296607fe540ca8
wyrtnn <135952609+wyrtnn@users.noreply.github.com>
2025-05-25 20:34:49 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,23 @@
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const handleLogin = async (email, password) => {
+  const response = await fetch("https://backend-black-luck-2.onrender.com/auth/login", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      email,
+      password
+    })
+  });
+
+  const data = await response.json();
+  console.log(data);
+};
+
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+  handleLogin(email, password); });
