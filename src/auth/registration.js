a354cf31bb0d5fa88aa85a1e6231ea9190f579df
wyrtnn <135952609+wyrtnn@users.noreply.github.com>
2025-05-29 12:18:56 +0200

    fix

--- src/auth/registration.js
+++ src/auth/registration.js
@@ -1,20 +1,22 @@
-const handleRegister = async (email, password) => {
-    const response = await fetch("https://backend-black-luck-2.onrender.com/auth/register", {
-      method: "POST",
-      credentials: "include",
-      headers: {
+const handleLogin = async (email, password) => {
+  const response = await fetch("https://backend-black-luck-2.onrender.com/auth/register", {
+    method: "POST",
+    credentials: "include",
+    headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ email, password })
-    });
-  
-    const data = await response.json();
-    console.log(data);
-  };
-  
-  document.getElementById("registerForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = document.getElementById("registerEmail").value;
-    const password = document.getElementById("registerPassword").value;
-    handleRegister(email, password);
+    body: JSON.stringify({ username, email, password })
   });
+
+  const data = await response.json();
+  console.log(data);
+};
+
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  handleLogin(username, email, password);
+});
